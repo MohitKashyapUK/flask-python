6d26733f43dd5fdd8b5be7509cef35e10b82e356
Mohit <mohitkashyap46707@gmail.com>
2023-10-03 19:25:51 +0530

    updating

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,28 @@ app = Flask(__name__)
 def hello_world():
   return "<p>Hello, World!</p>"
 
+@app.route("/bot-handler/telegram", methods=["GET", "POST"])
+def handler_telegram():
+  # import "./bot-handler/telegram"
+  import requests
+  url = "https://sk-results.000webhostapp.com/do.php"
+  params = { "data": request.json }
+  requests.get(url, params)
+  return "True"
+
+@app.get("/api/yt")
+def yt_api():
+  from urllib.parse import unquote
+  from pytube import YouTube
+  yt_url = request.args.get("url")
+  if not yt_url: return "Please spacify YouTube URL"
+  url = unquote(yt_url) # "https://youtu.be/FqAsaEVE2XY?si=Yp6_9nSdQpXJ4leP"
+  yt = YouTube(url)
+  streams = yt.streams
+  for stream in streams:
+    if stream.type == "video":
+      print(f"Quality: {stream.resolution} Size: {stream.filesize_mb}mb FPS: {stream.fps} Audio: {stream.is_progressive}")
+  return "Done!!!"
+
 if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0")
+  app.run(debug=True)
