bc3dc93f512171397d4b0f6bb64a4f1af9f6dfe6
Mohit <mohitkashyap46707@gmail.com>
2023-10-04 01:24:08 +0530

    Message handler added

--- app.py
+++ app.py
@@ -8,26 +8,23 @@ def hello_world():
 
 @app.route("/bot-handler/telegram", methods=["GET", "POST"])
 def handler_telegram():
-  # import "./bot-handler/telegram"
+  # Sending the JSON data
   import requests
   url = "https://sk-results.000webhostapp.com/do.php"
   params = { "data": request.get_data() }
   requests.get(url, params)
-  return "True"
-
-@app.get("/api/yt")
-def yt_api():
-  from urllib.parse import unquote
-  from pytube import YouTube
-  yt_url = request.args.get("url")
-  if not yt_url: return "Please spacify YouTube URL"
-  url = unquote(yt_url) # "https://youtu.be/FqAsaEVE2XY?si=Yp6_9nSdQpXJ4leP"
-  yt = YouTube(url)
-  streams = yt.streams
-  for stream in streams:
-    if stream.type == "video":
-      print(f"Quality: {stream.resolution} Size: {stream.filesize_mb}mb FPS: {stream.fps} Audio: {stream.is_progressive}")
-  return "Done!!!"
+  # Handling request
+  import telegram
+  data = request.get_json(force=True, silent=True)
+  if data:
+    local = locals()
+    for key in data: local[key] = data[key]
+    if local.get("message"): telegram.message(data)
+    elif local.get("callback_query"): telegram.callback_query(data)
+    else: return "This method is not allowed!"
+  else:
+    print("Wrong request data!")
+    return "False"
 
 if __name__ == "__main__":
   app.run(debug=True)
---
+++ telegram.py
@@ -0,0 +1,38 @@
+def message(data):
+  from pytube import YouTube, extract
+  import os, json, requests
+  Message = data.message
+  message_id = Message.message_id
+  chat_id = Message.from.id
+  text = Message.text
+  bot_api = "https://api.telegram.org"
+  bot_token = os.environ.get("bot_token")
+  method = "sendMessage"
+  params = { "chat_id": chat_id }
+  try:
+    video_id = extract.video_id(text)
+  except Exception as e:
+    video_id = None
+  url = f"{bot_api}/bot{bot_token}/{method}"
+  if video_id:
+    params["reply_markup"] = {
+      "inline_keyboard": [
+        [
+          {
+            "text": "Video",
+            "callback_data": json.dumps({ "yt_v": video_id })
+          }, {
+            "text": "Audio",
+            "callback_data": json.dumps({ "yt_a": video_id })
+          }
+        ], [
+          {
+            "text": "« Back",
+            "callback_data": json.dumps({ "yt": video_id })
+          }
+        ]
+      ]
+    }
+    requests.post(url, data=params)
+  else:
+    requests.post(url, data={ "chat_id": chat_id, "text": data })
